6c904337617c234940d8a3f0ed1135ce82879ada
Mike Dilley <mike.dilley@gmail.com>
2018-10-11 21:22:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,8 @@ def key_for_min_value(name_hash)
   min_key = ""
   name_hash.each do |key, value|
     min_value = value
-    if value < min_value    
-      min_key = key
-    end
+    min_key =key if value < min_value
+      
   end
   min_key
 end
