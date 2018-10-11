7cbc89a0c70b342f64e3094a5fd3656f1eafb942
Mike Dilley <mike.dilley@gmail.com>
2018-10-11 21:34:42 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
   min_value = Float::INFINITY
   name_hash.each do |key, value|
     min_key = key if value < min_value
+    min_value = value if value < min_valre
   end
   min_key
 end
